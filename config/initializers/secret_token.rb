96bb8002ee18ba91583f91f8a650f9952054f674
Ead Bannamol <ead@iwa.fi>
2013-11-06 15:18:51 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bicycle::Application.config.secret_key_base = '1d44797fa012c8ccffd3fccb42e464222aefe25f65bb1ec3740234455afc90bd45f01cfc6ea65c4970112e36f8478328f1261ea67534a4e3a38e3b519aca28e0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bicycle::Application.config.session_store :cookie_store, key: '_bicycle_session'
